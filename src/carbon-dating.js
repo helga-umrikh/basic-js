c407decd4694d7c74159aaccfaf32ec5d94c607c
helga-umrikh <zetaumr@gmail.com>
2023-04-18 02:05:38 +0300

    feat: add dateSimple function (`(date)`)

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,23 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample( sampleActivity ) {
+  if(typeof sampleActivity !== 'string') {return false};
+  if(sampleActivity.length === 0) {return false};
+  let regex = /^[\+\-]?\d*\.?\d+(?:[Ee][\+\-]?\d+)?$/;
+  if(regex.test(sampleActivity) === false) {return false};
+
+  let argNum = Number(sampleActivity)
+  if(argNum > MODERN_ACTIVITY) {return false};
+  if(argNum <= 0) {return false};
+
+
+  let a = MODERN_ACTIVITY / argNum;
+  let b = Math.log(a) * HALF_LIFE_PERIOD;
+  let c = b / Math.log(2);
+
+  let result = Math.ceil(c);
+  return result;
 }
 
 module.exports = {
